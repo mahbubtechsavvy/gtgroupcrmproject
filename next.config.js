a50e53587b61ba9c7b7491c3f09d17d83ce08d19
MD Mahbubur Rahman <85247674+mahbubtechsavvy@users.noreply.github.com>
2026-03-28 16:32:59 +0600

    add

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'kjppkkumublhiwzwufhe.supabase.co',
-        port: '',
+        port: '5432',
         pathname: '/storage/v1/object/public/**',
       },
     ],
